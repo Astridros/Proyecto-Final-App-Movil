d31e2e282f6a03d1ef5700952b67819afc130d6d
Alan De Leon <ad5913202@gmail.com>
2026-08-11 10:46:34 -0400

    feat: ampliar implementación de repositorio

--- lib/features/applications/data/repositories/applications_repository_impl.dart
+++ lib/features/applications/data/repositories/applications_repository_impl.dart
@@ -2,13 +2,52 @@ import '../../domain/entities/application.dart';
 import '../../domain/repositories/applications_repository.dart';
 import '../datasources/applications_remote_datasource.dart';
 
-class ApplicationsRepositoryImpl implements ApplicationsRepository {
-  const ApplicationsRepositoryImpl(this._remoteDataSource);
+class ApplicationsRepositoryImpl
+    implements ApplicationsRepository {
+  const ApplicationsRepositoryImpl(
+      this._remoteDataSource,
+      );
 
-  final ApplicationsRemoteDataSource _remoteDataSource;
+  final ApplicationsRemoteDataSource
+  _remoteDataSource;
 
   @override
-  Future<List<Application>> getMyApplications() {
-    return _remoteDataSource.getMyApplications();
+  Future<List<Application>>
+  getMyApplications() {
+    return _remoteDataSource
+        .getMyApplications();
+  }
+
+  @override
+  Future<List<Application>>
+  getOfferApplications(
+      String offerId,
+      ) {
+    return _remoteDataSource
+        .getOfferApplications(
+      offerId,
+    );
+  }
+
+  @override
+  Future<Application> updateApplication({
+    required String applicationId,
+    int? rating,
+    String? status,
+    double? salary,
+    String? currency,
+    DateTime? startDate,
+    String? duration,
+  }) {
+    return _remoteDataSource
+        .updateApplication(
+      applicationId: applicationId,
+      rating: rating,
+      status: status,
+      salary: salary,
+      currency: currency,
+      startDate: startDate,
+      duration: duration,
+    );
   }
 }
